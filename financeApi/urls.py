51e12cb427ef3a55764a9858217dd44adf72bd80
juancho391 <juanchischis2010@hotmail.com>
2025-06-27 12:23:53 -0500

    instalacion aplicacion transactions

--- financeApi/urls.py
+++ financeApi/urls.py
@@ -25,7 +25,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
+    path("api/", include("users.urls")),
+    path("api/", include("transactions.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
